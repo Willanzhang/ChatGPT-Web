4af1729729ef3b6affb960450a2b04fabcab36ad
Willanzhang <631871612@qq.com>
2023-02-22 16:22:06 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+      
+      <!-- <MainPage/> -->
+      <ByePage/>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+import ByePage from './components/ByePage.vue'
+export default {
+  name: 'App',
+  components: {
+    MainPage,
+    ByePage
+  },
+  data() {
+    return {
+      loading: true
+    };
+  },
+  created() {  
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  },
+  
+  methods:{
+    startLoading(){
+      this.loading = true
+    },
+    finishLoading(){
+      this.loading = false
+    }
+  }
+}
+</script>
+
